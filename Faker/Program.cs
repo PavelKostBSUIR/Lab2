86061554008877ae9421a9736f4c9d8e01ec9688
pk <p.k@gmail.com>
2021-11-23 20:05:17 +0300

    Initial commit

---
+++ Faker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    class Program
+    {
+        class A
+        {
+            public int integerNumber;
+            public string str;
+            public bool boolean;
+            public float floatNumber;
+            public override string ToString()
+            {
+                return $"int integerNumber = {integerNumber}, " +
+                    $"\nstring str = {str}, " +
+                    $"\nbool boolean = {boolean}, " +
+                    $"\nfloat floatNumber = {floatNumber}";
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Faker faker = new Faker();
+            A obj = faker.Create<A>();
+            Console.WriteLine(obj);
+            Console.WriteLine("List");
+            List<int> result = faker.Create<List<int>>();
+            if (result != null)
+            {
+                foreach(int i in result)
+                    Console.WriteLine(i);
+            }
+            else 
+            {
+                Console.WriteLine("NULL.");
+            }
+        }
+    }
+}
